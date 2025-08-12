c936f523f31ee0ab215d88373bf4e647b9053f5a
Igarmarkets <mrodriguez.loginlan@gmail.com>
2025-08-12 20:39:17 +0200

    Update init.ts

--- mani/lib/firebase/init.ts
+++ mani/lib/firebase/init.ts
@@ -1,44 +1,17 @@
-import 'react-native-get-random-values'
-import * as Notifications from 'expo-notifications'
-import { log } from 'components/logger'
-import { getApps, getApp, initializeApp, FirebaseApp } from 'firebase/app'
-import { CONFIGS } from 'common/envs/constants'
-import Constants from 'expo-constants'
-import { Auth, initializeAuth, getReactNativePersistence } from 'firebase/auth'
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
-import { getStorage } from 'firebase/storage'
+// web/lib/firebase/init.ts
+import { initializeApp, getApps, getApp } from 'firebase/app'
 
-export const ENV =
-  Constants.expoConfig?.extra?.eas.NEXT_PUBLIC_FIREBASE_ENV ?? 'PROD'
-export const ENV_CONFIG = CONFIGS[ENV]
-export const isProd = ENV === 'PROD'
-export const app = getApps().length
-  ? getApp()
-  : initializeApp(ENV_CONFIG.firebaseConfig)
-let initAuth: Auth | null = null
-
-export const getAuth = (app: FirebaseApp) => {
-  if (!initAuth) {
-    initAuth = initializeAuth(app, {
-      persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-    })
-  }
-  return initAuth
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 }
-export const auth = getAuth(app)
 
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-})
-export const storage = getStorage()
-export const privateStorage = getStorage(
-  app,
-  'gs://' + ENV_CONFIG.firebaseConfig.privateBucket
-)
+// Creamos una instancia única y la exportamos como default y como named
+const appInstance = getApps().length ? getApp() : initializeApp(firebaseConfig)
 
-log('using', ENV, 'env')
-log('env not switching? run `npx expo start --clear` and then try again')
+export default appInstance
+export const app = appInstance
