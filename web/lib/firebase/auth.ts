cf6b2e69a8653c643080bffda3f63dc8fa69909b
Igarmarkets <mrodriguez.loginlan@gmail.com>
2025-08-12 20:04:37 +0200

    Update auth.ts

--- web/lib/firebase/auth.ts
+++ web/lib/firebase/auth.ts
@@ -1,11 +1,5 @@
-import { Auth, getAuth } from 'firebase/auth'
-import { app } from './init'
+import { getAuth } from 'firebase/auth'
+import app from './init'
 
-let auth: Auth | null = null
-
-export const getFirebaseAuth = () => {
-  if (!auth) {
-    auth = getAuth(app)
-  }
-  return auth
-}
+export const auth = getAuth(app)
+export default auth
